c0d0743a56d12bd7cbec4c33504a2516270da405
williamsimms <williamsimms193@gmail.com>
2020-07-01 00:18:44 -0400

    Wise V1.2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
 const date = new Date().getFullYear()
+const rotate = document.getElementById('rotated')
+const circularText = document.querySelector('.circular-text')
+const homeLink = document.querySelector('.arrow')
+const hero = document.getElementById('hero')
+const photoContainer = document.querySelector('.mission-photo-container')
+const statementContainer = document.querySelector('.mission-statement')
+const scrollDownButton = document.querySelector('.scroll-down')
+const circle = new CircleType(rotate).radius(80)
+
+
+$(window).scroll(function() {
+    let offset = $(window).scrollTop()
+    offset = offset * 0.4
+
+    $('.circular-text').css({
+        transform:'rotate(' + offset + 'deg)'
+    })
+})
+
+homeLink.addEventListener('click',function(e) {
+    $('html, body').animate({scrollTop:0}, 1800)
+})
+
+window.addEventListener('scroll',function() {
+    scrollFn()
+})
+
+function scrollFn() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
+        circularText.style.opacity = 1
+        homeLink.style.opacity = 1
+    } else {
+        circularText.style.opacity = 0
+        homeLink.style.opacity = 0
+    }
+}
+
+function missionAnimate() {
+    if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+
+    }
+}
+
+scrollDownButton.addEventListener('click',function() {
+    $('html, body').animate({scrollTop:969}, 1800)
+})
+
+$('.navigation-bar a').on('click', function(e) {
+if (this.hash !== '') {
+    e.preventDefault()
+
+    const hash = this.hash
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    },1800)
+}
+})
