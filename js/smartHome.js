023d2067dee7be2e0efc25755b418e128873f080
williamsimms <williamsimms193@gmail.com>
2020-07-15 21:44:26 -0400

    added feature cards

--- js/smartHome.js
+++ js/smartHome.js
@@ -1,8 +1,8 @@
 const date = new Date()
-const videosSection = document.getElementById('video')
-const videoOne = document.querySelector('.videoOne')
 const featureSection = document.getElementById('features')
 const scrollDownButton = document.querySelector('.signature-scroll-button')
+const faders = document.querySelectorAll('.fade-in')
+const sliders = document.querySelectorAll('.slide-in')
 const currentDate = {
     '0': 'January',
     '1': 'February',
@@ -23,33 +23,57 @@ function setDate() {
 }
 setDate()
 
-let videosSectionTop = videosSection.getBoundingClientRect().top
+let featureSectionTop = featureSection.getBoundingClientRect().top
 scrollDownButton.addEventListener('click', function () {
     $('html, body').animate({
-        scrollTop: videosSectionTop
+        scrollTop: featureSectionTop
     }, 1000)
 })
 
-const controller = new ScrollMagic.Controller()
+const fadeOptions = {
+    root: null,
+    threshold: 0.5,
+    rootMargin: '0px 0px -100px 0px'
+}
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
 
-const scene = new ScrollMagic.Scene({
-        duration: 21000,
-        triggerElement: videosSection,
-        triggerHook: 0
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add('appear')
+            appearOnScroll.unobserve(entry.target)
+        }
     })
-    .setPin(videosSection)
-    .addTo(controller)
+
+}, fadeOptions)
 
 
-
-let scrollPos = 0
-
-scene.on('update', e => {
-    scrollPos = e.scrollPos / 1000
+faders.forEach((fader) => {
+    appearOnScroll.observe(fader)
 })
 
-setInterval(() => {
+//Slide 
+const slideOptions = {
+    root: null,
+    threshold: .2,
+    rootMargin: '0px 0px -100px 0px'
+}
+const appearOnScrollSlide = new IntersectionObserver(function (entries, appearOnScrollSlide) {
 
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add('translate')
+            appearOnScrollSlide.unobserve(entry.target)
+        }
+    })
 
-    videoOne.currentTime = scrollPos
-}, 33.3)
+}, slideOptions)
+
+// Change Slier Black On Scroll
+
+sliders.forEach((slider) => {
+    appearOnScrollSlide.observe(slider)
+})
